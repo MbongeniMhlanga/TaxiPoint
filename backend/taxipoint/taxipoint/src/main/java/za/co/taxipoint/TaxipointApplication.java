7c5ef3a24ad6a5e7bc68b13ec1cee3ac48abdbb0
MbongeniMhlanga <mbongeniroyce@gmail.com>
2025-08-12 19:02:12 +0200

    backend files upload
    backend files upload

---
+++ backend/taxipoint/taxipoint/src/main/java/za/co/taxipoint/TaxipointApplication.java
@@ -0,0 +1,13 @@
+package za.co.taxipoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxipointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxipointApplication.class, args);
+	}
+
+}
---
+++ backend/taxipoint/taxipoint/src/test/java/za/co/taxipoint/TaxipointApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.taxipoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxipointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
