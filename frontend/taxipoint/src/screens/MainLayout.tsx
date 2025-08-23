152aea797437920c3139ce3dd0b9ded916f55243
MbongeniMhlanga <mbongeniroyce@gmail.com>
2025-08-23 16:54:17 +0200

    update

--- frontend/taxipoint/src/screens/MainLayout.tsx
+++ frontend/taxipoint/src/screens/MainLayout.tsx
@@ -42,7 +42,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({
       {/* Dark overlay */}
       {isSidebarOpen && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          className="fixed inset-0 bg-black bg-opacity-50 z-2001"
           onClick={() => setIsSidebarOpen(false)}
         />
       )}
