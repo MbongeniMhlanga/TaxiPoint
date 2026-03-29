6e6270dda9409525bc8746ccae1854e558e27507
Mbongeni Mhlanga <mbongeniroyce@gmail.com>
2026-03-29 21:02:03 +0200

    Update withAndroidResolutionPatch.js

--- mobile/TaxiPoint/withAndroidResolutionPatch.js
+++ mobile/TaxiPoint/withAndroidResolutionPatch.js
@@ -7,12 +7,16 @@ module.exports = function withAndroidResolutionPatch(config) {
       configurations.all {
           resolutionStrategy {
               force "androidx.versionedparcelable:versionedparcelable:1.1.1"
-              force "androidx.annotation:annotation:1.7.0"
-              force "androidx.core:core:1.12.0"
-              force "androidx.core:core-ktx:1.12.0"
-              force "androidx.appcompat:appcompat:1.6.1"
-              force "androidx.activity:activity:1.8.0"
-              force "androidx.fragment:fragment:1.6.1"
+              force "androidx.annotation:annotation:1.9.0"
+              force "androidx.core:core:1.15.0"
+              force "androidx.core:core-ktx:1.15.0"
+              force "androidx.appcompat:appcompat:1.7.0"
+              force "androidx.activity:activity:1.9.2"
+              force "androidx.activity:activity-ktx:1.9.2"
+              force "androidx.fragment:fragment:1.8.4"
+              force "androidx.fragment:fragment-ktx:1.8.4"
+              force "androidx.lifecycle:lifecycle-runtime:2.8.6"
+              force "androidx.lifecycle:lifecycle-viewmodel:2.8.6"
               force "androidx.legacy:legacy-support-v4:1.0.0"
           }
           exclude group: 'com.android.support', module: 'support-v4'
@@ -23,6 +27,13 @@ module.exports = function withAndroidResolutionPatch(config) {
           exclude group: 'com.android.support', module: 'support-fragment'
           exclude group: 'com.android.support', module: 'versionedparcelable'
       }
+      
+      dependencies {
+          implementation "androidx.core:core:1.15.0"
+          implementation "androidx.activity:activity:1.9.2"
+          implementation "androidx.fragment:fragment:1.8.4"
+          implementation "androidx.appcompat:appcompat:1.7.0"
+      }
       `;
       
       if (!config.modResults.contents.includes('resolutionStrategy')) {
@@ -32,5 +43,6 @@ module.exports = function withAndroidResolutionPatch(config) {
     return config;
   });
 };
+
 
 
