fb61e53aa2757fadacd31ae5c9071a07b15b01c2
Mbongeni Mhlanga <mbongeniroyce@gmail.com>
2026-03-29 19:48:52 +0200

    build fix

--- mobile/TaxiPoint/withAndroidResolutionPatch.js
+++ mobile/TaxiPoint/withAndroidResolutionPatch.js
@@ -9,7 +9,16 @@ module.exports = function withAndroidResolutionPatch(config) {
               force "androidx.versionedparcelable:versionedparcelable:1.1.1"
               force "androidx.annotation:annotation:1.2.0"
               force "androidx.core:core:1.6.0"
+              force "androidx.appcompat:appcompat:1.3.1"
+              force "androidx.legacy:legacy-support-v4:1.0.0"
           }
+          exclude group: 'com.android.support', module: 'support-v4'
+          exclude group: 'com.android.support', module: 'support-compat'
+          exclude group: 'com.android.support', module: 'support-media-compat'
+          exclude group: 'com.android.support', module: 'support-core-utils'
+          exclude group: 'com.android.support', module: 'support-core-ui'
+          exclude group: 'com.android.support', module: 'support-fragment'
+          exclude group: 'com.android.support', module: 'versionedparcelable'
       }
       `;
       
@@ -20,3 +29,4 @@ module.exports = function withAndroidResolutionPatch(config) {
     return config;
   });
 };
+
