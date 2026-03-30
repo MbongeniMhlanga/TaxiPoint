4362997e6291a3c121abf713bacc6513932b10c3
Mbongeni Mhlanga <mbongeniroyce@gmail.com>
2026-03-30 20:10:10 +0200

    build fixes

--- mobile/TaxiPoint/babel.config.js
+++ mobile/TaxiPoint/babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [], // Reanimated 4/RN 0.81 handles this internally; manual plugin often causes startup crashes on New Arch
   };
 };
